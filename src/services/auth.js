d12da4ad733ff23f19f2afa18597f2f3d46c9bc3
tiw25999 <tiw25999@gmail.com>
2025-08-12 23:18:35 +0700

    for deploy

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+const API_URL = 'https://license-plate-system.onrender.com';
 
 const authClient = axios.create({
   baseURL: API_URL,
